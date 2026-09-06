8aedc1c98cefe0b4a67f39e793dd3ec258f8c9ae
pandeyaditi8b-code <pandeyaditi8b@gmail.com>
2026-09-06 14:52:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-// Fake news question 
-
+// Fake News
 function fakeNews() {
     document.getElementById("fakeAnswer").innerHTML =
-    "Do not click the link. First Check the sender and verify the information from a reliable source.";
+        "Do not click the link. Check the sender and verify the information from a reliable source.";
 }
+
 
 // Quiz
-
 function quizAnswer(answer) {
-    if(answer === "no"){
+
+    if (answer === "no") {
         document.getElementById("quizAnswer").innerHTML =
-        "Correct! OTP should never be shared with anyone.";
-    }else{
+            "Correct! OTP should never be shared with anyone.";
+    } else {
         document.getElementById("quizAnswer").innerHTML =
-        "Wrong! Never share your OTP with anyone.";
+            "Wrong! Never share your OTP with anyone.";
     }
+
 }
 
-//Survey
-document.getElementById("surveyForm").addEventListener("submit", function(event){
+
+// Feedback Form
+document.getElementById("feedbackForm").addEventListener("submit", function(event) {
+
     event.preventDefault();
 
-    let name = document.getElementById("name").ariaValueMax;
+    let name = document.getElementById("feedbackName").value;
 
-    document.getElementById("message").innerHTML =
-    "Thank you, " + name + "! Your response has been recorded.";
+    document.getElementById("feedbackMessage").innerHTML =
+        "Thank you, " + name + "! Your feedback has been submitted.";
 
-    document.getElementById("surveyForm").requestFullscreen();
+    document.getElementById("feedbackForm").reset();
+
 });
